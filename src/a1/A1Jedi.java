e4d407f48d375bf2ef959aa244b3735ca079544f
brianbritt <brianbritt@brians-mbp-5.lan>
2019-08-31 20:30:28 -0400

    Jedi Attempt 1 - Passes test

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,68 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numberOfItems = scan.nextInt();
+		String[] itemNames = new String[numberOfItems];
+		int[] numberPurchased = new int[numberOfItems];
+		int[] customersBought = new int[numberOfItems];
+		
+		
+		for (int i = 0; i < numberPurchased.length; i++) {
+			numberPurchased[i] = 0;
+		}
+		
+		for (int i = 0; i < customersBought.length; i++) {
+			customersBought[i] = 0;
+		}
+		
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			itemNames[i] = scan.next();
+			scan.next();
+		}
+		
+		int numberOfCustomers = scan.nextInt();
+		
+		for (int i = 0; i < numberOfCustomers; i++) {
+			scan.next();
+			scan.next();
+			
+			int numberOfDifferentItems = scan.nextInt();
+			
+			
+			for (int j = 0; j < numberOfDifferentItems; j++) {
+				int itemQuantity = scan.nextInt();
+				String itemName = scan.next();
+				int numberPurchasedIndex = 0;
+				
+				for (int k = 0; k < itemNames.length; k++) {
+					if (itemName.equals(itemNames[k])) {
+						numberPurchasedIndex = k;
+						customersBought[k] ++;
+						break;
+					}
+				
+			
+				
+				}
+				
+				numberPurchased[numberPurchasedIndex] += itemQuantity;
+				
+			}
+		}
+		
+		
+		
+		
+		
+		for (int i = 0; i < itemNames.length; i++) {
+			if (numberPurchased[i] == 0) {
+				System.out.println("No customers bought " + itemNames[i]);
+			} else {
+				System.out.println(customersBought[i] + " customers bought " + numberPurchased[i] +  " " + itemNames[i]);
+			}
+		}
+		
 		
 	}
 }
